86750f36cb7e24924fed5d9137f8440c9c200b4f
Ben Cipollini <bcipollini@classy.org>
2017-03-05 12:42:26 -0800

    AFQV: initial commit to pull AFQ-Browser into AFQVault

--- afqvault/apps/main/views.py
+++ afqvault/apps/main/views.py
@@ -8,6 +8,6 @@ def index_view(request):
     recent_collections = Collection.objects.exclude(DOI__isnull=True).exclude(private=True).order_by('-doi_add_date')
     #this is faster than using annotate and count!
     recent_collections = [col for col in recent_collections if col.basecollectionitem_set.count() > 0][:10]
-    
+
     context = {'recent_collections': recent_collections}
-    return render(request, 'index.html.haml', context)
+    return render(request, 'index.html', context)
